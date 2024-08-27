711a74defbcb6bdbf9fdb97a8fcc8135219af67c
VDRBreno <vdr.breno0149@outlook.com>
2024-08-27 19:54:47 -0300

    Add ListMeasure route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,12 @@ import { RouteOptions } from 'fastify';
 
 import UploadController from './Upload';
 import ConfirmController from './Confirm';
+import ListMeasureController from './ListMeasure';
 
 const routes: RouteOptions[] = [
   UploadController,
-  ConfirmController
+  ConfirmController,
+  ListMeasureController
 ];
 
 export default routes;
