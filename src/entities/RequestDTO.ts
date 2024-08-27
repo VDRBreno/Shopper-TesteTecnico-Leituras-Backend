83e3e60c3d9365e8db2bd329c4a18358fba43f8d
VDRBreno <vdr.breno0149@outlook.com>
2024-08-27 18:22:55 -0300

    Change error type to string

--- src/entities/RequestDTO.ts
+++ src/entities/RequestDTO.ts
@@ -2,14 +2,14 @@ import Joi from 'joi';
 
 export default abstract class RequestDTO<DTO> {
 
-  public error: Joi.ValidationError | undefined;
+  public error: string | undefined;
   public value: DTO | undefined;
 
   abstract validate(data: unknown): data is DTO;
 
   assign(data: Joi.ValidationResult) {
     if(data.error)
-      this.error = data.error;
+      this.error = data.error.message;
     else
       this.value = data.value;
   }
